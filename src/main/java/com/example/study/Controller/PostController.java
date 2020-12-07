b6a9ebcc0dfa6e297108f1793452e19a37178c3b
xonmin <han51361@naver.com>
2020-12-07 15:57:52 +0900

    Lombok 적용

--- src/main/java/com/example/study/Controller/PostController.java
+++ src/main/java/com/example/study/Controller/PostController.java
@@ -2,10 +2,7 @@ package com.example.study.Controller;
 
 
 import com.example.study.model.SearchParam;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
@@ -15,12 +12,31 @@ public class PostController {
     // ajax 검색
     // http post body -> data
     //json, xml, multipart-form / text-plain 형태 다 가능
+    /*
+    *   Rest의 개념
+    *  http 프로토콜에 있는 method를 활용한 아키텍처 스타일
+    * http method 를 톤ㅇ해서 resource 를 처리
+    * crud 를 통한 resource 조작시, 사용
+    *
+    * */
 
-
-  @PostMapping(value = "/postMethod")
-    public String postMethod(@RequestBody SearchParam searchParam){
+    @PostMapping(value = "/postMethod")
+    public SearchParam postMethod(@RequestBody SearchParam searchParam){
       // body 에 담긴 data를 받는다.
 
-      return "OK";
+      return searchParam;
   }
+
+  @PutMapping("/putMethod")
+  public void put(){
+
+
+  }
+
+
+  @PatchMapping("/patchMethod")
+  public void patch(){
+
+  }
+
 }
