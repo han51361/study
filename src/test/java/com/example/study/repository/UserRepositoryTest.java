0285b5fa7f7d462e1e1bebc3908e0fe63ba71695
xonmin <han51361@naver.com>
2020-12-08 22:57:40 +0900

    jpa를 이용한 crud 완료

--- src/test/java/com/example/study/repository/UserRepositoryTest.java
+++ src/test/java/com/example/study/repository/UserRepositoryTest.java
@@ -1,12 +1,16 @@
 package com.example.study.repository;
 
-import com.example.study.StudyApplication;
+
 import com.example.study.StudyApplicationTests;
 import com.example.study.model.entity.User;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-
+import org.junit.jupiter.api.Assertions;
+import javax.transaction.Transactional;
 import java.time.LocalDateTime;
+import java.util.Optional;
+
+
 
 public class UserRepositoryTest extends StudyApplicationTests {
 
@@ -29,15 +33,41 @@ public class UserRepositoryTest extends StudyApplicationTests {
         System.out.println("newUser : "+newUser);
     }
 
+    @Test
     public void read(){
+       Optional< User> user = userRepository.findById(2L); //id type 이 long이라
 
+      user.ifPresent(selectUser->{
+           System.out.println("user: "+selectUser);
+       });
     }
 
+    @Test
     public  void update(){
+        Optional< User> user = userRepository.findById(2L); //id type 이 long이라
+
+        user.ifPresent(selectUser->{
+            selectUser.setAccount("ppppp");
+            selectUser.setUpdatedAt(LocalDateTime.now());
+            selectUser.setUpdatedBy("update method()");
+
+            userRepository.save(selectUser);
+        });
 
     }
-
+    //@DeleteMapping("/api/user")
+    @Test
+    @Transactional
     public void delete(){
+        Optional<User>user = userRepository.findById(1L);
+
+        Assertions.assertTrue(user.isPresent()); //true
+        user.ifPresent(selectUser->{
+            userRepository.delete(selectUser);
+        });
+
+        Optional<User>deleteUser = userRepository.findById(1L);
+      Assertions.assertFalse(deleteUser.isPresent()); //false
 
 
     }
