1a689cbafb52fe50886dbe768116742deedb3314
xonmin <han51361@naver.com>
2020-12-11 15:10:15 +0900

    Category User table test

--- src/test/java/com/example/study/repository/UserRepositoryTest.java
+++ src/test/java/com/example/study/repository/UserRepositoryTest.java
@@ -23,60 +23,39 @@ public class UserRepositoryTest extends StudyApplicationTests {
     public void create(){
         User user = new User(); //Singleton
 
-        user.setAccount("TestUser01");
-        user.setEmail("TestUser@gmail.com");
-        user.setPhoneNumber("010-1111-1111");
-        user.setCreatedAt(LocalDateTime.now());
-        user.setCreatedBy("admin");
+       String account = "Test01";
+       String password = "Test01";
+       String status = "Registered";
+       String email = "Test01@gmail.com";
+       String phoneNumber = "010-1111-2222";
+       LocalDateTime registeredAt = LocalDateTime.now();
+       LocalDateTime createdAt = LocalDateTime.now();
+       String createdBy = "AdminServer";
+
+    user.setAccount(account);
+    user.setCreatedBy(createdBy);
+    user.setPhoneNumber(phoneNumber);
+    user.setPassword(password);
+    user.setStatus(status);
+    user.setEmail(email);
+    user.setRegisteredAt(registeredAt);
+    user.setCreatedAt(createdAt);
+
 
 
        User newUser = userRepository.save(user);
-        System.out.println("newUser : "+newUser);
+        Assertions.assertNotNull(newUser);
     }
 
     @Test
     @Transactional
     public void read(){
-       Optional<User> user = userRepository.findByAccount("TestUser01"); //id type 이 long이라
+        User user = userRepository.findFirstByPhoneNumberOrderByIdDesc("010-1111-2222");
 
-      user.ifPresent(selectUser->{
-         selectUser.getOrderDetailList().stream().forEach(detail -> {
-             Item item  =detail.getItem();
-             System.out.println(item);
-         });
-
-       });
-    }
-
-    @Test
-    public  void update(){
-        Optional< User> user = userRepository.findById(4L); //id type 이 long이라
-
-        user.ifPresent(selectUser->{
-            selectUser.setAccount("ppppp");
-            selectUser.setUpdatedAt(LocalDateTime.now());
-            selectUser.setUpdatedBy("update method()");
-
-            userRepository.save(selectUser);
-        });
+        Assertions.assertNotNull(user);
 
     }
-    //@DeleteMapping("/api/user")
-    @Test
-    @Transactional
-    public void delete(){
-        Optional<User>user = userRepository.findById(1L);
 
-        Assertions.assertTrue(user.isPresent()); //true
-        user.ifPresent(selectUser->{
-            userRepository.delete(selectUser);
-        });
-
-        Optional<User>deleteUser = userRepository.findById(1L);
-      Assertions.assertFalse(deleteUser.isPresent()); //false
-
-
-    }
 
 
 }
