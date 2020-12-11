fb967d1a20ba7701e878ce21fceb3190dbae437f
xonmin <han51361@naver.com>
2020-12-11 14:39:24 +0900

    entity 및 repository 설정 2

--- src/main/java/com/example/study/repository/CategoryRepository.java
+++ src/main/java/com/example/study/repository/CategoryRepository.java
@@ -1,15 +1,10 @@
 package com.example.study.repository;
 
-import com.example.study.model.entity.User;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-
-import java.util.Optional;
-
-
 @Repository
-public interface UserRepository extends JpaRepository<User,Long> {
-
+public interface CategoryRepository extends JpaRepository<CategoryRepository, Long> {
 
 }
