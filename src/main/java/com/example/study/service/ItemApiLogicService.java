37184bc0ab8e2df7a7bc66909d4764d73d6f34e7
xonmin <han51361@naver.com>
2020-12-22 23:07:06 +0900

    12.22 36 product api create 완료

---
+++ src/main/java/com/example/study/service/ItemApiLogicService.java
@@ -0,0 +1,83 @@
+package com.example.study.service;
+
+import com.example.study.ifs.CrudInterface;
+import com.example.study.model.entity.Item;
+import com.example.study.model.entity.Partner;
+import com.example.study.model.network.Header;
+import com.example.study.model.network.request.ItemApiRequest;
+import com.example.study.model.network.response.ItemApiResponse;
+import com.example.study.repository.ItemRepository;
+import com.example.study.repository.PartnerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Service
+public class ItemApiLogicService implements CrudInterface<ItemApiRequest, ItemApiResponse> {
+
+    @Autowired
+    private PartnerRepository partnerRepository;
+
+    @Autowired
+    private ItemRepository itemRepository;
+    @Override
+    public Header<ItemApiResponse> create(Header<ItemApiRequest> request) {
+        // 1.  request data
+        ItemApiRequest itemApiRequest = request.getData();
+
+        //2 . item 생성
+        Item item  = Item.builder()
+                .status(itemApiRequest.getStatus())
+                .name(itemApiRequest.getName())
+                .title(itemApiRequest.getTitle())
+                .content(itemApiRequest.getContent())
+                .price(itemApiRequest.getPrice())
+                .brandName(itemApiRequest.getBrandName())
+                .registeredAt(LocalDateTime.now())
+                .partner(partnerRepository.getOne(itemApiRequest.getPartnerId()))
+                .build();
+
+        Item newItem = itemRepository.save(item);
+
+        //3. 생성된 데이터 -> itemRepository return
+
+        return response(newItem);
+
+    }
+
+    @Override
+    public Header<ItemApiResponse> read(Long id) {
+        return null;
+    }
+
+    @Override
+    public Header<ItemApiResponse> update(Header<ItemApiRequest> request) {
+        return null;
+    }
+
+    @Override
+    public Header delete(Long id) {
+        return null;
+    }
+
+
+    private Header<ItemApiResponse> response(Item item){
+
+        ItemApiResponse itemApiResponse = ItemApiResponse.builder()
+                .id(item.getId())
+                .status(item.getStatus())
+                .name(item.getName())
+                .title(item.getTitle())
+                .content(item.getContent())
+                .price(item.getPrice())
+                .brandName(item.getBrandName())
+                .registeredAt(item.getRegisteredAt())
+                .unregisteredAt(item.getUnregisteredAt())
+                .partnerId(item.getPartner().getId())
+                .build();
+
+
+        return Header.OK(itemApiResponse);
+    }
+}
