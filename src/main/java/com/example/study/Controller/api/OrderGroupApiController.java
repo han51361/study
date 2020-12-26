7fa3ef2ce97b018020eb4a4ed91a625c4ae13db9
xonmin <han51361@naver.com>
2020-12-26 21:18:10 +0900

    12.26 ordergroup service logic 완성

--- src/main/java/com/example/study/Controller/api/OrderGroupApiController.java
+++ src/main/java/com/example/study/Controller/api/OrderGroupApiController.java
@@ -4,38 +4,42 @@ import com.example.study.ifs.CrudInterface;
 import com.example.study.model.network.Header;
 import com.example.study.model.network.request.OrderGroupApiRequest;
 import com.example.study.model.network.response.OrderGroupApiResponse;
+import com.example.study.service.OrderGroupApiLogicService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 @Slf4j
 @RestController
 @RequestMapping("/api/ordergroup")
 public class OrderGroupApiController implements CrudInterface<OrderGroupApiRequest, OrderGroupApiResponse> {
 
+    @Autowired
+    private OrderGroupApiLogicService orderGroupApiLogicService;
 
     @Override
     @PostMapping("")
     public Header<OrderGroupApiResponse> create(@RequestBody Header<OrderGroupApiRequest> request) {
 
-        return null;
+        return orderGroupApiLogicService.create(request);
     }
 
     @Override
     @GetMapping({"id"})
     public Header<OrderGroupApiResponse> read(@PathVariable Long id) {
         log.info("read id : {}", id);
-        return null;
+        return orderGroupApiLogicService.read(id);
     }
 
     @Override
     @PutMapping("")
     public Header<OrderGroupApiResponse> update(@RequestBody  Header<OrderGroupApiRequest> request) {
-        return null;
+        return orderGroupApiLogicService.update(request);
     }
 
     @Override
     @DeleteMapping({"id"})
     public Header delete(@PathVariable Long id) {
         log.info("delete : {} ", id );
-        return null;
+        return orderGroupApiLogicService.delete(id);
     }
 }
